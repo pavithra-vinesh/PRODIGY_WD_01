737cacaaf080194df5bb649dba53f8880e42e020
pavithra_vinesh <pavithrav2104@gmail.com>
2026-06-03 13:38:09 +0530

    Initial Project upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const navbar = document.querySelector('#navbar');
+
+window.addEventListener('scroll', () => {
+    
+    if (window.scrollY > 80) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
